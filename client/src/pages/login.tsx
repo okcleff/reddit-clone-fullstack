6ef4d65102db8bb6b510af08be73bdc134e407ee
okcleff <okcleff@gmail.com>
2023-06-23 10:13:20 +0900

    로그인 페이지 및 기능 생성, 로그인 시 cookie 처리

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,38 +1,35 @@
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
 import React, { FormEvent, useState } from "react";
 import InputGroup from "../components/InputGroup";
-import { useAuthState } from "../context/auth";
+import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useAuthDispatch, useAuthState } from "../context/auth";
 
-const Register = () => {
-  const [email, setEmail] = useState("");
+const Login = () => {
+  let router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState<any>({});
   const { authenticated } = useAuthState();
+  const dispatch = useAuthDispatch();
 
-  let router = useRouter();
   if (authenticated) router.push("/");
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-
     try {
       const res = await axios.post(
-        "/auth/register",
-        {
-          email,
-          password,
-          username,
-        },
+        "/auth/login",
+        { password, username },
         { withCredentials: true }
       );
-      console.log("res", res);
-      router.push("/login");
+
+      dispatch("LOGIN", res.data?.user);
+
+      router.push("/");
     } catch (error: any) {
-      console.log("error", error);
-      setErrors(error.response.data || {});
+      console.log(error);
+      setErrors(error.response?.data || {});
     }
   };
 
@@ -40,14 +37,8 @@ const Register = () => {
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
-          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+          <h1 className="mb-2 text-lg font-medium">로그인</h1>
           <form onSubmit={handleSubmit}>
-            <InputGroup
-              placeholder="Email"
-              value={email}
-              setValue={setEmail}
-              error={errors.email}
-            />
             <InputGroup
               placeholder="Username"
               value={username}
@@ -62,14 +53,14 @@ const Register = () => {
               error={errors.password}
             />
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원 가입
+              로그인
             </button>
           </form>
 
           <small>
-            이미 가입하셨나요?
-            <Link href="/login">
-              <a className="ml-1 text-blue-500 uppercase">로그인</a>
+            아직 아이디가 없나요?
+            <Link href="/register">
+              <a className="ml-1 text-blue-500 uppercase">회원가입</a>
             </Link>
           </small>
         </div>
@@ -78,4 +69,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
