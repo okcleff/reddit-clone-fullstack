3273abd7923a0e12c24f2b77d621959b0ccbea84
woncheolok <okcleff@gmail.com>
2023-06-25 15:14:48 +0900

    커뮤니티 페이지 생성하기(NavBar)

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,25 @@
-import '../styles/globals.css';
+import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import Axios from 'axios';
 import { AuthProvider } from '../context/auth';
+import NavBar from '../components/NavBar';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
   Axios.defaults.withCredentials = true;
 
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login'];
+  const authRoute = authRoutes.includes(pathname);
+
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? '' : 'pt-12 bg-gray-200 min-h-screen'}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
