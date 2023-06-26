5377908af6c34fb75bd31780ddedf10b0cba91cc
okcleff <okcleff@gmail.com>
2023-06-26 11:31:46 +0900

    useSWR을 이용한 커뮤니티 리스트 가져오기 및 커뮤니티 리스트 UI 생성

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -75,25 +75,25 @@ const createSub = async (req: Request, res: Response, next) => {
   }
 };
 
-// const topSubs = async (req: Request, res: Response) => {
-//   try {
-//     const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' ||s."imageUrn",'https://www.gravatar.com/avatar?d=mp&f=y')`;
-//     const subs = await AppDataSource.createQueryBuilder()
-//       .select(
-//         `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
-//       )
-//       .from(Sub, "s")
-//       .leftJoin(Post, "p", `s.name = p."subName"`)
-//       .groupBy('s.title, s.name, "imageUrl"')
-//       .orderBy(`"postCount"`, "DESC")
-//       .limit(5)
-//       .execute();
-//     return res.json(subs);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json({ error: "문제가 발생했습니다." });
-//   }
-// };
+const topSubs = async (req: Request, res: Response) => {
+  try {
+    const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn",'https://www.gravatar.com/avatar?d=mp&f=y')`;
+    const subs = await AppDataSource.createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, 's')
+      .leftJoin(Post, 'p', `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, 'DESC')
+      .limit(5)
+      .execute();
+    return res.json(subs);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: '문제가 발생했습니다.' });
+  }
+};
 
 // const ownSub = async (req: Request, res: Response, next: NextFunction) => {
 //   const user: User = res.locals.user;
@@ -181,7 +181,7 @@ const router = Router();
 
 // router.get("/:name", userMiddleware, getSub);
 router.post('/', userMiddleware, authMiddleware, createSub);
-// router.get("/sub/topSubs", topSubs);
+router.get('/sub/topSubs', topSubs);
 // router.post(
 //   "/:name/upload",
 //   userMiddleware,
