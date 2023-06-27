7973381e9d320ed70342a38a4385bc45b2cb1326
okcleff <okcleff@gmail.com>
2023-06-27 15:21:45 +0900

    포스트와 댓글에 Vote 기능 구현

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -44,6 +44,31 @@ const PostPage = () => {
     }
   };
 
+  const vote = async (value: number, comment?: Comment) => {
+    if (!authenticated) router.push('/login');
+
+    // 이미 클릭 한 vote 버튼을 눌렀을 시에는 reset
+    if (
+      (!comment && value === post?.userVote) ||
+      (comment && comment.userVote === value)
+    ) {
+      value = 0;
+    }
+
+    try {
+      await axios.post('/votes', {
+        identifier,
+        slug,
+        commentIdentifier: comment?.identifier,
+        value,
+      });
+      postMutate();
+      commentMutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex max-w-5xl px-4 pt-5 mx-auto">
       <div className="w-full md:mr-3 md:w-8/12">
@@ -51,6 +76,36 @@ const PostPage = () => {
           {post && (
             <>
               <div className="flex">
+                {/* 좋아요 싫어요 기능 부분 */}
+                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                  {/* 좋아요 */}
+                  <div
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    onClick={() => vote(1)}
+                  >
+                    {post.userVote === 1 ? (
+                      <FaArrowUp className="text-red-500" />
+                    ) : (
+                      <FaArrowUp />
+                    )}
+                  </div>
+
+                  {/* 좋아요 + 싫어요 스코어 */}
+                  <p className="text-xs font-bold">{post.voteScore}</p>
+
+                  {/* 싫어요 */}
+                  <div
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                    onClick={() => vote(-1)}
+                  >
+                    {post.userVote === -1 ? (
+                      <FaArrowDown className="text-blue-500" />
+                    ) : (
+                      <FaArrowDown />
+                    )}
+                  </div>
+                </div>
+
                 <div className="py-2 pr-2">
                   <div className="flex items-center">
                     <p className="text-xs test-gray-400">
@@ -84,7 +139,7 @@ const PostPage = () => {
               </div>
 
               {/* 댓글 작성 구간 */}
-              <div className="pr-6 mb-4">
+              <div className="pr-6 mb-4 pl-10">
                 {authenticated ? (
                   <div>
                     <p className="mb-1 text-xs">
@@ -133,6 +188,36 @@ const PostPage = () => {
               {/* 댓글 리스트 부분 */}
               {comments?.map((comment) => (
                 <div className="flex" key={comment.identifier}>
+                  {/* 좋아요 싫어요 기능 부분 */}
+                  <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                    {/* 좋아요 */}
+                    <div
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      onClick={() => vote(1, comment)}
+                    >
+                      {comment.userVote === 1 ? (
+                        <FaArrowUp className="text-red-500" />
+                      ) : (
+                        <FaArrowUp />
+                      )}
+                    </div>
+
+                    {/* 좋아요 + 싫어요 스코어 */}
+                    <p className="text-xs font-bold">{comment.voteScore}</p>
+
+                    {/* 싫어요 */}
+                    <div
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                      onClick={() => vote(-1, comment)}
+                    >
+                      {comment.userVote === -1 ? (
+                        <FaArrowDown className="text-red-500" />
+                      ) : (
+                        <FaArrowDown />
+                      )}
+                    </div>
+                  </div>
+
                   <div className="py-2 pr-2">
                     <p className="mb-1 text-xs leading-none">
                       <Link href={`/u/${comment.username}`}>
@@ -142,9 +227,10 @@ const PostPage = () => {
                       </Link>
 
                       <span className="text-gray-600">
-                        {`${comment.voteScore}
-                        posts
-                        ${dayjs(comment.createdAt).format('YYYY-MM-DD HH:mm')}
+                        {`
+                          ${comment.voteScore}
+                          posts
+                          ${dayjs(comment.createdAt).format('YYYY-MM-DD HH:mm')}
                         `}
                       </span>
                     </p>
